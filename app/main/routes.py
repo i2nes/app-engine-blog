457b8e097ae067d4753f341fe48af47beffc5624
i2nes <32749894+i2nes@users.noreply.github.com>
2017-10-15 01:17:56 +0100

    Added configurable blog settings

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,62 +1,63 @@
 from . import app
 from flask import render_template
+from config import blog_config
 
 
 @app.route('/')
 def home():
 
     context = {
-        'title': 'Clean Blog - Start Bootstrap Theme'
+        'title': 'Clean Blog - Start Bootstrap Theme',
     }
 
-    return render_template('main/home_page.html', context=context)
+    return render_template('main/home_page.html', context=context, blog_config=blog_config)
 
 
 @app.route('about/')
 def about():
 
     context = {
-        'title': 'About Page'
+        'title': 'About Page',
     }
 
-    return render_template('main/about_page.html', context=context)
+    return render_template('main/about_page.html', context=context, blog_config=blog_config)
 
 
 @app.route('contact/')
 def contact():
 
     context = {
-        'title': 'Contact Page'
+        'title': 'Contact Page',
     }
 
-    return render_template('main/contact_page.html', context=context)
+    return render_template('main/contact_page.html', context=context, blog_config=blog_config)
 
 
 @app.route('post/')
 def post():
 
     context = {
-        'title': 'Post Page'
+        'title': 'Post Page',
     }
 
-    return render_template('main/post_page.html', context=context)
+    return render_template('main/post_page.html', context=context, blog_config=blog_config)
 
 
 @app.app_errorhandler(404)
 def page_not_found(e):
 
     context = {
-        'title': 'Page Not Found'
+        'title': 'Page Not Found',
     }
 
-    return render_template('main/404.html', context=context), 404
+    return render_template('main/404.html', context=context, blog_config=blog_config), 404
 
 
 @app.app_errorhandler(500)
 def page_not_found(e):
 
     context = {
-        'title': 'Server Error'
+        'title': 'Server Error',
     }
 
-    return render_template('main/500.html', context=context), 500
+    return render_template('main/500.html', context=context, blog_config=blog_config), 500
--- config.py
+++ config.py
@@ -1,7 +1,23 @@
 import os
 
-
+# Flask App Configurations
 config = {
+
     'DEBUG': True if os.getenv('SERVER_SOFTWARE', '').startswith('Development/') else False,
     'SECRET_KEY': 'Some big sentence',
+}
+
+# Blog Configurations
+blog_config = {
+
+    'BLOG_NAME': 'Start Bootstrap',
+    'BLOG_TITLE_LINE_1': 'Clean Blog',
+    'BLOG_TITLE_LINE_2': 'A Blog Theme by Start Bootstrap',
+    'COPYRIGHT': 'Your Website 2017',
+
+    # Comment out any social links you don't want to display
+    'FACEBOOK_LINK': 'https://www.facebook.com/',
+    'TWITTER_LINK': 'https://twitter.com/',
+    'GITHUB_LINK': 'https://github.com/i2nes/app-engine-blog',
+
 }
